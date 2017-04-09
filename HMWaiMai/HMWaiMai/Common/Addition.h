06a7a04ac3aae574094a45e404c841bb6b5378aa
zxc842947859 <zhangxc1120@qq.com>
2017-04-09 14:02:52 +0800

    01-项目准备

---
+++ HMWaiMai/HMWaiMai/Common/Addition.h
@@ -0,0 +1,17 @@
+//
+//  Addition.h
+//  HMWaiMai
+//
+//  Created by chao on 2017/4/9.
+//  Copyright © 2017年 chao. All rights reserved.
+//
+
+#ifndef Addition_h
+#define Addition_h
+
+
+
+#import "UIColor+Addition.h"
+
+
+#endif /* Addition_h */
---
+++ HMWaiMai/HMWaiMai/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMWaiMai
+//
+//  Created by chao on 2017/4/9.
+//  Copyright © 2017年 chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
